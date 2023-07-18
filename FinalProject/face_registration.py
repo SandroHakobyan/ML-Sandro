e9db863768431e693b51415232a1a80c4b34b10b
SandroHakobyan <sandro.hakobyan.y@tumo.org>
2023-07-18 11:53:03 +0400

    Second Day on Final Project

--- FinalProject/face_registration.py
+++ FinalProject/face_registration.py
@@ -34,6 +34,38 @@ while True:
 		face_encodings = face_recognition.face_encodings(rgb_frame, [(y, x+w, y+h, x)])
 
 		for face_encoding in face_encodings:
-			face_data.append({"name": name, "face": frame[y:y+h, x:x+w], "face_encoding": face_encoding, "access": access_list}
-)
+			face_data.append({"name": name, "face": frame[y:y+h, x:x+w], "face_encoding": face_encoding, "access": access_list})
+
+
+	cv2.imshow('Register Face', frame)
+	
+	if cv2.waitKey(1) & 0xFF == ord(s):
+		capture_count +=1
+		print(f"Capture (capture_count) complete!")
+
+
+	if capture_Count >=5:
+		break
+
+
+
+
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+now = datetime.now()
+file_name = f"faces/{now.strftime('%Y-%m-%d-%H-%M-%s')}-{name}.pickle"
+with open(file_name, 'wb') as f:
+	pickle.dump(face_data, f)
+print(f"Face data for '{name}' saved Successfully!")
+
+
+
+
+
+
+
+
 
